1b1d068cac7e143a95d8a9fdc42137040be15707
taro-hida <sk8trou@gmail.com>
2021-08-13 19:04:03 +0900

    featute/7vtpfj https://v3.ja.vuejs.org/guide/data-methods.html まで履修

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const vm = createApp(App).mount('#app')
+console.log(vm.$data)
+
+// import Increments from './components/Increments.vue'
+// const app = createApp(Increments)
+// const vm = app.mount('#app')
+// setInterval(function(){ vm.increment() }, 1000);
+
