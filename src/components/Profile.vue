ce4c1940df773c6c96fa2fda1a60d19fa8459671
taro-hida <sk8trou@gmail.com>
2021-08-25 19:03:12 +0900

    featute/esa354 dropdown-caret つきのプロフィールを実装

---
+++ src/components/Profile.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="dropdown">
+      <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+          <span>
+          <img
+              src="https://img.icons8.com/dusk/100/000000/user-female-circle.png"
+              alt=""
+              class="user-avatar-md rounded-circle"
+              width="40"
+              height="40"
+          >
+          </span>
+      </a>
+      <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+        <li><a class="dropdown-item" href="#">Action</a></li>
+        <li><a class="dropdown-item" href="#">Another action</a></li>
+        <li><a class="dropdown-item" href="#">Something else here</a></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Profile"
+}
+</script>
+
+<style scoped>
+
+</style>
