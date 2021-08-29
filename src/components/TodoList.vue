93df4d33393910f27a880d8e7d797a97fc91fa4f
taro-hida <sk8trou@gmail.com>
2021-08-29 20:56:46 +0900

    feature/esa354 編集ボタン削除

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -76,7 +76,6 @@
                 </td>
                 <td>{{task}}</td>
                 <td class="p-0 text-center text-right">
-                  <a class="btn btn-primary btn-action mr-1" data-toggle="tooltip" title="" data-original-title="Edit"><i class="fas fa-pencil-alt"></i></a>
                   <a class="btn btn-danger btn-action" data-toggle="tooltip" title="" data-confirm="Are You Sure?|This action can not be undone. Do you want to continue?" data-confirm-yes="alert('Deleted')" data-original-title="Delete"><i class="fas fa-trash"></i></a>
                 </td>
               </tr>
